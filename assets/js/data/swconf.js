fe36ae1e79e46382a9725333a3b5dcabc9f320ea
lxriscute0501 <lxriscute0501@users.noreply.github.com>
2025-07-12 09:37:29 +0000

    deploy: db62061169c22801caa191b42493c63e0d7548e6

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752170832',resources: [
+    cacheName: 'chirpy-1752313049',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
